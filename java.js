e25f6d28bd70923b20213ac46289953e66506afb
Eric Van <ggclarence@yahoo.com>
2022-02-15 12:24:53 -0600

    Test

---
+++ java.js
@@ -0,0 +1,67 @@
+const aboutUs = document.getElementById("about-us")
+
+function button(){
+    const resetBtn = document.createElement("button")
+    resetBtn.innerHTML = "go back"
+    resetBtn.addEventListener("click", () => {
+        window.location.reload();
+
+    })
+aboutUs.append(resetBtn)
+}
+
+document.querySelectorAll("img").forEach(item => {
+    item.addEventListener(`click`, (e) => {
+        if (e.target.id === "ev-picture"){
+            document.querySelector("ul").innerText = ""
+            const ericPic = document.createElement("img")
+                ericPic.src = "https://i.imgur.com/H0ljfjr.jpg"
+                ericPic.id = "focus" 
+            const ericText = document.createElement("p")
+                ericText.innerText = `Hi, my name is Eric nice to meet you!. On this project I worked on the "About" part of this project aswell as the "games" section of the webpage.`
+
+             button()
+
+            
+            aboutUs.append(ericPic,ericText)
+        }
+        if (e.target.id === "eg-picture"){
+            const egSrc = e.target.src
+            const egPic = document.createElement("img")
+            const egText = document.createElement("p")
+            document.querySelector("ul").innerText = ""
+            
+            egText.innerText = "Hi my name is slim shady"
+            egPic.id = "focus"
+            egPic.src = egSrc
+            button()
+            aboutUs.append(egPic,egText)
+            
+        }
+            
+        if (e.target.id === "ahmed-picture"){
+            document.querySelector("ul").innerText = ""
+            const ahmedPic = document.createElement("img")
+                ahmedPic.src = "https://ca.slack-edge.com/T02MD9XTF-U02T65UV1LJ-8db8dd41e386-512"
+                ahmedPic.id = "focus"
+            const ahmedFocus = document.createElement("p")
+                ahmedFocus.innerText = `Hi, my name is slim shady`
+                button()
+            aboutUs.append(ahmedPic,ahmedFocus)
+        }
+        if (e.target.id === "nathaly-picture"){
+            document.querySelector("ul").innerText = ""
+            const nathalyPic = document.createElement("img")
+                nathalyPic.src = "https://ca.slack-edge.com/T02MD9XTF-U02G9ELQQ8P-1505ae46e619-512"
+                nathalyPic.id = "focus"
+            const natFocus = document.createElement("p")
+                natFocus.innerText = `Hi, my name is slim shady`
+                button()
+            aboutUs.append(nathalyPic,natFocus)
+            
+        }
+    })
+})
+
+
+// test.forEach(element => console.log(element));
