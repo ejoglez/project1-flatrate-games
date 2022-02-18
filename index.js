8ce3629d7054964a0a594f03133d89c439ab6dab
Eric Van <ggclarence@yahoo.com>
2022-02-17 23:27:45 -0600

    added event listeners that when hover over your picture displays text +hidden message #BEST

--- index.js
+++ index.js
@@ -1,68 +1,97 @@
 const aboutUs = document.getElementById("about-us")
 
-function button(){
-    const resetBtn = document.createElement("button")
-    resetBtn.id = "resetBtn"
-    resetBtn.innerHTML = "go back"
-    resetBtn.addEventListener("click", () => {
-        window.location.reload();
+// function button(){
+//     const resetBtn = document.createElement("button")
+//     resetBtn.id = "resetBtn"
+//     resetBtn.innerHTML = "go back"
+//     resetBtn.addEventListener("click", () => {
+//         window.location.reload();
 
-    })
-aboutUs.append(resetBtn)
+//     })
+// aboutUs.append(resetBtn)
+// }
+
+// document.querySelectorAll("img").forEach(item => {
+//     item.addEventListener(`click`, (e) => {
+//         if (e.target.id === "ev-picture"){
+//             document.querySelector("ul").innerText = ""
+//             const ericPic = document.createElement("img")
+//                 ericPic.src = "AboutUsPics/21125505_1532294940154732_1754325753808962236_o.jpg"
+//                 ericPic.id = "focus" 
+//             const ericText = document.createElement("p")
+//                 ericText.innerText = `Hi, my name is Eric nice to meet you!. On this project I worked on the "About" part of this project aswell as the "games" section of the webpage.`
+
+//              button()
+
+            
+//             aboutUs.append(ericPic,ericText)
+//         }
+//         if (e.target.id === "eg-picture"){
+//             const egSrc = e.target.src
+//             const egPic = document.createElement("img")
+//             const egText = document.createElement("p")
+//             document.querySelector("ul").innerText = ""
+            
+//             egText.innerText = "Hi my name is slim shady"
+//             egPic.id = "focus"
+//             egPic.src = egSrc
+//             button()
+//             aboutUs.append(egPic,egText)
+            
+//         }
+            
+//         if (e.target.id === "ahmed-picture"){
+//             document.querySelector("ul").innerText = ""
+//             const ahmedPic = document.createElement("img")
+//                 ahmedPic.src = "AboutUsPics/T02MD9XTF-U02T65UV1LJ-8db8dd41e386-512.jpg"
+//                 ahmedPic.id = "focus"
+//             const ahmedFocus = document.createElement("p")
+//                 ahmedFocus.innerText = `Hi, my name is slim shady`
+//                 button()
+//             aboutUs.append(ahmedPic,ahmedFocus)
+//         }
+//         if (e.target.id === "nathaly-picture"){
+//             document.querySelector("ul").innerText = ""
+//             const nathalyPic = document.createElement("img")
+//                 nathalyPic.src = "AboutUsPics/T02MD9XTF-U02G9ELQQ8P-1505ae46e619-512.jpg"
+//                 nathalyPic.id = "focus"
+//             const natFocus = document.createElement("p")
+//                 natFocus.innerText = `Hi, my name is slim shady`
+//                 button()
+//             aboutUs.append(nathalyPic,natFocus)
+            
+//         }
+//     })
+// })
+const hoverText = document.getElementById("hoverText")
+imageDisplay()
+function imageDisplay(){
+ let  imageTest = document.querySelectorAll(".image__overlay")
+imageTest.forEach(imageNode => imageFunc(imageNode))
 }
 
-document.querySelectorAll("img").forEach(item => {
-    item.addEventListener(`click`, (e) => {
-        if (e.target.id === "ev-picture"){
-            document.querySelector("ul").innerText = ""
-            const ericPic = document.createElement("img")
-                ericPic.src = "AboutUsPics/21125505_1532294940154732_1754325753808962236_o.jpg"
-                ericPic.id = "focus" 
-            const ericText = document.createElement("p")
-                ericText.innerText = `Hi, my name is Eric nice to meet you!. On this project I worked on the "About" part of this project aswell as the "games" section of the webpage.`
-
-             button()
-
-            
-            aboutUs.append(ericPic,ericText)
+function imageFunc(images){
+    images.addEventListener("mouseover",(event)=>{
+        if(event.target.id === "EV"){
+            hoverText.innerText = "Hi, I'm Eric Van"
         }
-        if (e.target.id === "eg-picture"){
-            const egSrc = e.target.src
-            const egPic = document.createElement("img")
-            const egText = document.createElement("p")
-            document.querySelector("ul").innerText = ""
-            
-            egText.innerText = "Hi my name is slim shady"
-            egPic.id = "focus"
-            egPic.src = egSrc
-            button()
-            aboutUs.append(egPic,egText)
-            
+        if(event.target.id === "EG"){
+            hoverText.innerText = "Hi, I'm Eric Gonzalez"
         }
-            
-        if (e.target.id === "ahmed-picture"){
-            document.querySelector("ul").innerText = ""
-            const ahmedPic = document.createElement("img")
-                ahmedPic.src = "AboutUsPics/T02MD9XTF-U02T65UV1LJ-8db8dd41e386-512.jpg"
-                ahmedPic.id = "focus"
-            const ahmedFocus = document.createElement("p")
-                ahmedFocus.innerText = `Hi, my name is slim shady`
-                button()
-            aboutUs.append(ahmedPic,ahmedFocus)
+        if(event.target.id === "ahmed"){
+            hoverText.innerText = "Hi, I'm Ahemd"
         }
-        if (e.target.id === "nathaly-picture"){
-            document.querySelector("ul").innerText = ""
-            const nathalyPic = document.createElement("img")
-                nathalyPic.src = "AboutUsPics/T02MD9XTF-U02G9ELQQ8P-1505ae46e619-512.jpg"
-                nathalyPic.id = "focus"
-            const natFocus = document.createElement("p")
-                natFocus.innerText = `Hi, my name is slim shady`
-                button()
-            aboutUs.append(nathalyPic,natFocus)
-            
+        if(event.target.id === "nat"){
+            hoverText.innerText = "Hi, I'm Nat"
         }
     })
-})
+    images.addEventListener("mouseleave",()=>{
+        hoverText.innerHTML = ""
+    })
+}
+
+// cartBttn.addEventListener("click",()=>{
+//     alert("Greetings\n Item is in the cart!")
 
 const gameUrl="https://www.cheapshark.com/api/1.0/games?ids=13,36,43,59,75,77,80,123,38,53,99,15,21,25"
 
